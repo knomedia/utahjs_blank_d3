b725ce6550012935ef96dbf66a94c3657b250cf2
jsk <knomedia@gmail.com>
2013-07-02 16:49:08 -0600

    Switch to margin convention
    http://bl.ocks.org/mbostock/3019563

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 (function(){
   var svg,
-      padding = 20,
-      w = 960,
-      h = 450,
+      margin = {top: 40, right: 20, bottom: 20, left: 20},
       xScale,
       yScale,
+      w = 960 - margin.left - margin.right,
+      h = 450 - margin.top - margin.bottom,
       dataset = [29, 59, 19, 58, 89]
 
   function init () {
@@ -14,10 +14,10 @@
 
   function createSvg () {
     svg = d3.select("#chart").append("svg")
-      .attr("width", w)
-      .attr("height", h)
-      .style("background-color", "#13373a");
-    svg = svg.append("g");
+      .attr("width", w + margin.left + margin.right)
+      .attr("height", h + margin.top + margin.right);
+    svg = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
 
   function drawElements () {
